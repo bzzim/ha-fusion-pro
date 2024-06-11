eb3b8a973012a0d0459f927c5a3d71c66b97cd4b
Bzz <i@bzz.im>
2024-06-11 17:04:19 +0500

    Translations

--- scripts/translations/generate.sh
+++ scripts/translations/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-remote="ssh matte@192.168.1.241 -p 55"
+remote="ssh bzz@192.168.100.150 -i ~/Documents/keys/private.pem"
 path="$(dirname "$(readlink -f "$0")")"
 
-$remote "/usr/local/bin/docker exec -i homeassistant bash -c 'python3'" < "$path/fetch.py" | \
+$remote "docker exec -i homeassistant bash -c 'python3'" < "$path/fetch.py" | \
 python3 "$path/split.py" "$path/../../static/translations/"
